1693681ac2404abfe3d1c43efeea33ced92644ea
Carl Yan <94885426+Carl7yan@users.noreply.github.com>
2023-11-02 13:38:26 +0800

    Update apb_driver.sv

--- uvm_tb/apb_driver.sv
+++ uvm_tb/apb_driver.sv
@@ -4,8 +4,8 @@ class apb_driver extends uvm_driver #(apb_transaction);
 	`uvm_component_utils(apb_driver)
 
 	virtual apb_if				vifapb;
-  uart_config 				cfg; // Handle to  a cfg class
-	apb_config 					apb_cfg;
+ 	uart_config 				cfg; // Handle to  a cfg class
+	apb_config 				apb_cfg;
 
 	function new (string name, uvm_component parent);
 		super.new(name, parent);
@@ -18,46 +18,37 @@ class apb_driver extends uvm_driver #(apb_transaction);
 
 endclass
 
-  	// ---------------------------------------
-  	// build phase
-  	// ---------------------------------------
+
   	function void apb_driver::build_phase(uvm_phase phase);
   		super.build_phase(phase);
-		  if(!uvm_config_db#(uart_config)::get(this, "", "cfg", cfg))
-		  	`uvm_fatal("No cfg",{"Configuration must be set for: ",get_full_name(),".cfg"});
-		  if(!uvm_config_db#(apb_config)::get(this, "", "apb_cfg", apb_cfg))
-		  	`uvm_fatal("No apb_cfg",{"Configuration must be set for: ",get_full_name(),".cfg"});
+		if(!uvm_config_db#(uart_config)::get(this, "", "cfg", cfg))
+			`uvm_fatal("No cfg",{"Configuration must be set for: ",get_full_name(),".cfg"});
+		if(!uvm_config_db#(apb_config)::get(this, "", "apb_cfg", apb_cfg))
+			`uvm_fatal("No apb_cfg",{"Configuration must be set for: ",get_full_name(),".cfg"});
   	endfunction: build_phase
 
-	  // ---------------------------------------
-  	// Conenct phase
-  	// ---------------------------------------
-	  function void apb_driver::connect_phase(uvm_phase phase);
-      super.connect_phase(phase);
+	function void apb_driver::connect_phase(uvm_phase phase);
+      		super.connect_phase(phase);
 	   	if(!uvm_config_db#(virtual apb_if)::get(this, "", "vifapb", vifapb))
-      	`uvm_fatal("NO_VIF",{"virtual interface must be set for: ",get_full_name(),".vifapb"});
-    endfunction : connect_phase
+      			`uvm_fatal("NO_VIF",{"virtual interface must be set for: ",get_full_name(),".vifapb"});
+    	endfunction : connect_phase
 
-  	// ---------------------------------------
-  	//  run phase
-  	// ---------------------------------------
   	task apb_driver::run_phase(uvm_phase phase);
   		apb_transaction req;
-  	 	forever
-  	 	begin
+  	 	forever begin
 		     @(posedge vifapb.PCLK iff (vifapb.PRESETn))
 		     `DRIVAPB_IF.PSELx		<= 0;
 		     `DRIVAPB_IF.PENABLE	<= 0;
 
 		     `DRIVAPB_IF.PWRITE		<= 0;
-  	     `DRIVAPB_IF.PWDATA		<= 0;
-  	     `DRIVAPB_IF.PADDR		<= 0;
+  		     `DRIVAPB_IF.PWDATA		<= 0;
+  	     	     `DRIVAPB_IF.PADDR		<= 0;
 
-  	     seq_item_port.get_next_item(req);
-  	     drive(req);
- 		     `uvm_info("APB_DRIVER_TR", $sformatf("APB Finished Driving Transfer \n%s",req.sprint()), UVM_HIGH)
-  	     seq_item_port.item_done();
-  	 	end
+	  	     seq_item_port.get_next_item(req);
+	  	     drive(req);
+	 	     `uvm_info("APB_DRIVER_TR", $sformatf("APB Finished Driving Transfer \n%s",req.sprint()), UVM_HIGH)
+	  	     seq_item_port.item_done();
+	  	 end
   	endtask : run_phase
 
   	//---------------------------------------------------------
@@ -66,26 +57,25 @@ endclass
   	//--------------------------------------------------------
 
   	task apb_driver::drive(apb_transaction req);
-
-		  `DRIVAPB_IF.PSELx			    <= apb_cfg.psel_Index;
-  	  `DRIVAPB_IF.PWRITE			  <= req.PWRITE;
-		  if(req.PADDR == cfg.baud_config_addr)
-		  	`DRIVAPB_IF.PWDATA			<= cfg.bRate;
-		  else if (req.PADDR == cfg.frame_config_addr)
-		  	`DRIVAPB_IF.PWDATA			<= cfg.frame_len;
-		  else if (req.PADDR == cfg.parity_config_addr)
+		`DRIVAPB_IF.PSELx			  <= apb_cfg.psel_Index;
+  	  	`DRIVAPB_IF.PWRITE			  <= req.PWRITE;
+		if(req.PADDR == cfg.baud_config_addr)
+			`DRIVAPB_IF.PWDATA			<= cfg.bRate;
+		else if (req.PADDR == cfg.frame_config_addr)
+			`DRIVAPB_IF.PWDATA			<= cfg.frame_len;
+		else if (req.PADDR == cfg.parity_config_addr)
 		  	`DRIVAPB_IF.PWDATA			<= cfg.parity;
-		  else if (req.PADDR == cfg.stop_bits_config_addr)
+		else if (req.PADDR == cfg.stop_bits_config_addr)
 		  	`DRIVAPB_IF.PWDATA			<= cfg.n_sb;
-		  else
+		else
 		  	`DRIVAPB_IF.PWDATA			<= req.PWDATA;
-  	  `DRIVAPB_IF.PADDR			    <= req.PADDR;
+  	 	`DRIVAPB_IF.PADDR			    	<= req.PADDR;
 
-		  @(posedge vifapb.PCLK);
-		  `DRIVAPB_IF.PENABLE			  <= 1;
+		@(posedge vifapb.PCLK);
+		`DRIVAPB_IF.PENABLE			  	<= 1;
 
-		  wait(`DRIVAPB_IF.PREADY);
-		  `DRIVAPB_IF.PSELx			    <= 0;
-		  `DRIVAPB_IF.PENABLE			  <= 0;
-		  wait(!`DRIVAPB_IF.PREADY);
+		wait(`DRIVAPB_IF.PREADY);
+		`DRIVAPB_IF.PSELx			    	<= 0;
+		`DRIVAPB_IF.PENABLE			  	<= 0;
+		wait(!`DRIVAPB_IF.PREADY);
   	endtask
